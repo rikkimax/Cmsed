72742b666bc71f9e8e379bccc6354385710cb362
rikki cattermole <pbdadmin@gmail.com>
2014-01-18 23:45:47 +1300

    Ahh the basics of odata's view support done!

--- source/cmsed/base/odata.d
+++ source/cmsed/base/odata.d
@@ -55,8 +55,9 @@ interface ODataFilters {
  */
 mixin template ODataRoute(ushort protection, TYPES...) {
 	import vibe.http.status;
-	import dvorm.util : dbIgnore;
+	import dvorm.util : dbIgnore, getNameValue;
 	import std.conv : to;
+	//pragma(msg, getODataOneValue!(protection, TYPES)());
 	mixin(getODataOneValue!(protection, TYPES)());
 }
 
@@ -105,10 +106,52 @@ protected {
 				//   application/xml
 				//   application/atom+xml
 				//  default to error 406 Not Acceptable
+				ret ~="import std.file:append;append(\"out.txt\", \"got \" ~ http_request.params[\"key)\"][0 .. $-1] ~ \"\\n\");";
+				ret ~= "        switch(http_request.query.get(\"format\", null)) {\n";
+				ret ~= "            case \"json\":\n";
+				ret ~= "                string jsonValue = \"{\";\n";
+				ret ~= "                jsonValue ~= \"\\\"d\\\": {\";\n";
+				ret ~= "                jsonValue ~= \"\\\"results\\\": [{\";\n";
+				foreach (m; __traits(allMembers, C)) {
+					static if (isUsable!(C, m)() && !shouldBeIgnored!(C, m)()) {
+						static if (is(typeof(mixin("c." ~ m)) : Object)) {
+							foreach (n; __traits(allMembers, typeof(mixin("c." ~ m)))) {
+								static if (isUsable!(typeof(mixin("c." ~ m)), n)() && !shouldBeIgnored!(typeof(mixin("c." ~ m)), n)()) {
+									static if (isAnId!(typeof(mixin("c." ~ m)), n)) {
+										static if (is(typeof(mixin("c." ~ m ~ "." ~ n)): Object)) {
+											assert(0, "Data models cannot have objects with objects as id's.");
+										} else static if (is(typeof(mixin("c." ~ m ~ "." ~ n)) == string) ||
+										                  is(typeof(mixin("c." ~ m ~ "." ~ n)) == dstring) ||
+										                  is(typeof(mixin("c." ~ m ~ "." ~ n)) == wstring)) {
+											ret ~= "                jsonValue ~= getNameValue!(typeof(value), \"" ~ m ~ "\") ~ \"_\" ~ getNameValue!(typeof(value." ~ m ~ "), \"" ~ n ~ "\") ~ \": \\\"\" ~ value." ~ m ~ "." ~ n ~ "~ \"\\\", \";\n";
+										} else static if (typeof(mixin("c." ~ m ~ "." ~ n)).stringof != "void") {
+											ret ~= "                jsonValue ~= getNameValue!(typeof(value), \"" ~ m ~ "\") ~ \"_\" ~ getNameValue!(typeof(value." ~ m ~ "), \"" ~ n ~ "\") ~ \": \" ~ to!string(value." ~ m ~ "." ~ n ~ ") ~ \", \";\n";
+										}
+									}
+								}
+							}
+						} else static if (is(typeof(mixin("c." ~ m)) == string) ||
+						                  is(typeof(mixin("c." ~ m)) == dstring) ||
+						                  is(typeof(mixin("c." ~ m)) == wstring)) {
+							ret ~= "                jsonValue ~= getNameValue!(typeof(value), \"" ~ m ~ "\") ~ \": \\\"\" ~ value." ~ m ~ " ~ \"\\\", \";\n";
+						} else static if (typeof(mixin("c." ~ m)).stringof != "void") {
+							ret ~= "                jsonValue ~= getNameValue!(typeof(value), \"" ~ m ~ "\") ~ \": \" ~ to!string(value." ~ m ~ ") ~ \", \";\n";
+						}
+					}
+				}
+				ret ~= "                jsonValue.length -= 2;\n";
+				ret ~= "                jsonValue ~= \"}], \\\"__count\\\": 1}}\";\n";
+				ret ~= "                http_response.writeBody(cast(string)jsonValue, \"application/json\");";
+				ret ~= "                break;\n";
+				ret ~= "            default:\n";
+				ret ~= "                http_response.statusCode = cast(HTTPStatus)406;\n";
+				ret ~= "                break;\n";
+				ret ~= "        }\n";
 				
 				// check which properties are not allowed.
 				// don't output them
 				// output rest
+				
 				
 				ret ~= "    }\n";
 				ret ~= "}\n";
@@ -180,15 +223,6 @@ protected {
 								// if not exist do the following:
 								
 								// output it
-								// what format should we output to?
-								//  is it ?format=json
-								//  is it ?format=xml
-								//  is it ?format=atom
-								//  if not specified check for accept header for:
-								//   application/json
-								//   application/xml
-								//   application/atom+xml
-								//  default to error 406 Not Acceptable
 								
 								// check which properties are not allowed.
 								// don't output them
