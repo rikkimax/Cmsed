50407d941ca0f2422300ff573c0a36d5b7de3111
rikki cattermole <pbdadmin@gmail.com>
2014-01-19 18:31:20 +1300

    Forgot to register the user models.

--- source/cmsed/user/models/package.d
+++ source/cmsed/user/models/package.d
@@ -2,7 +2,11 @@ module cmsed.user.models;
 import cmsed.base;
 
 public import cmsed.user.models.user;
+public import cmsed.user.models.usergroup;
+public import cmsed.user.models.group;
 
 shared static this() {
 	registerModel!UserModel;
+	registerModel!UserGroupModel;
+	registerModel!GroupModel;
 }
