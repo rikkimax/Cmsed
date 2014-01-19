8a88433d75c7578be1c2f429412a7fce726860d4
rikki cattermole <pbdadmin@gmail.com>
2014-01-19 22:31:14 +1300

    This is why you test type stuff out.

--- source/cmsed/base/caches/settings.d
+++ source/cmsed/base/caches/settings.d
@@ -9,7 +9,7 @@ import cmsed.base.cache;
 
 mixin CacheManager!(SystemSettingModel, "getSystemSettings", true);
 
-shared(SystemSettingModel) getSettingByName(string name) {
+SystemSettingModel getSettingByName(string name) {
 	synchronized {
 		foreach(s; getSystemSettings) {
 			if (s.name == name)
