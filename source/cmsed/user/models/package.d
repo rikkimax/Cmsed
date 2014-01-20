f7a5acf6e71c1a4c8ac24dbe979e70bc00d3c858
rikki cattermole <pbdadmin@gmail.com>
2014-01-20 18:23:49 +1300

    Registering all data models is a good idea no?

--- source/cmsed/user/models/package.d
+++ source/cmsed/user/models/package.d
@@ -4,9 +4,11 @@ import cmsed.base;
 public import cmsed.user.models.user;
 public import cmsed.user.models.usergroup;
 public import cmsed.user.models.group;
+public import cmsed.user.models.userauth;
 
 shared static this() {
 	registerModel!UserModel;
 	registerModel!UserGroupModel;
 	registerModel!GroupModel;
+	registerModel!UserAuthModel;
 }
