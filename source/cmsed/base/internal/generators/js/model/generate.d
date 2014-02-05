5f0056852b4ce83f583b990a384809b59ae42233
rikki cattermole <pbdadmin@gmail.com>
2014-02-05 23:29:26 +1300

    Refactoring out the actual implementation of the javascript generation into own library files.

--- source/cmsed/base/internal/generators/js/model/generate.d
+++ source/cmsed/base/internal/generators/js/model/generate.d
@@ -1,5 +1,6 @@
 module cmsed.base.internal.generators.js.model.generate;
 import cmsed.base.internal.generators.js.model.defs;
+import cmsed.base.internal.generators.js.model.jsface;
 import dvorm.util;
 import std.traits : isBasicType, isBoolean;
 
@@ -50,8 +51,7 @@ string generateJsFunc(T, ushort ajaxProtection)() {
 void handleClassStart(T, ushort ajaxProtection, T t = newValueOfType!T)(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
 	switch(oopHandler) {
 		case OOPHandler.JSFace:
-			ret ~= "var " ~ getTableName!T ~ " = Class({\n";
-			constructorArgs ~= "    constructor: function(";
+			handleClassStartJSFace!(T, ajaxProtection)(ret, constructorArgs, constructorSet, props);
 			break;
 		default:
 			break;
@@ -64,28 +64,7 @@ void handleClassStart(T, ushort ajaxProtection, T t = newValueOfType!T)(ref stri
 void handleClassEnd(T, ushort ajaxProtection, T t = newValueOfType!T)(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
 	switch(oopHandler) {
 		case OOPHandler.JSFace:
-			if (constructorArgs[$-2] == ',')
-				constructorArgs.length -= 2;
-			constructorArgs ~= ") {\n";
-			
-			ret ~= constructorArgs;
-			ret ~= constructorSet;
-			ret ~= "    },\n";
-			
-			if (props.length > 2) {
-				if (props[$-2] == ',') {
-					props.length -= 2;
-					props ~= '\n';
-				}
-			}
-			ret ~= props;
-			
-			if (ret[$-2] == ',') {
-				ret.length -= 2;
-				ret ~= '\n';
-			}
-			
-			ret ~= "});";
+			handleClassEndJSFace!(T, ajaxProtection)(ret, constructorArgs, constructorSet, props);
 			break;
 		default:
 			break;
@@ -100,17 +79,7 @@ void handleClassPropertyObjectProperty(T, ushort ajaxProtection, string m, strin
                                        )(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
 	switch(oopHandler) {
 		case OOPHandler.JSFace:
-			string name1 = getNameValue!(T, m);
-			string name2 = getNameValue!(U, n);
-			
-			constructorArgs ~= name1 ~ "_" ~ name2 ~ ", ";
-			static if (isBoolean!V) {
-				constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = Boolean(" ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ");\n";
-			} else static if (isBasicType!V) {
-				constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = Number(" ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ");\n";
-			} else {
-				constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = " ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ";\n";
-			}
+			handleClassPropertyObjectPropertyJSFace!(T, ajaxProtection, m, n)(ret, constructorArgs, constructorSet, props);
 			break;
 		default:
 			break;
@@ -125,19 +94,7 @@ void handleClassProperty(T, ushort ajaxProtection, string m, // params
                          )(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
 	switch(oopHandler) {
 		case OOPHandler.JSFace:
-			string name = getNameValue!(T, m);
-			if (name == "")
-				name = "_";
-			
-			constructorArgs ~= name ~ ", ";
-			
-			static if (isBoolean!U) {
-				constructorSet ~= "        this." ~ name ~ " = Boolean(" ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ");\n";
-			} else static if (isBasicType!U) {
-				constructorSet ~= "        this." ~ name ~ " = Number(" ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ");\n";
-			} else {
-				constructorSet ~= "        this." ~ name ~ " = " ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ";\n";
-			}
+			handleClassPropertyJSFace!(T, ajaxProtection, m)(ret, constructorArgs, constructorSet, props);
 			break;
 		default:
 			break;
@@ -152,43 +109,7 @@ void handleClassPropertyRelationship(T, ushort ajaxProtection, string m, // para
                                      )(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
 	switch(oopHandler) {
 		case OOPHandler.JSFace:
-			mixin("import " ~ getRelationshipClassModuleName!(T, m) ~ ";");
-			string name = getTableName!(mixin(getRelationshipClassName!(T, m)));
-			
-			props ~= "    " ~ getterName!(getNameValue!(T, m)) ~ ": function() {\n";
-			props ~= "        return " ~ name ~ ".findOne(";
-			
-			foreach(n; __traits(allMembers, U)) {
-				static if (isUsable!(U, n) && !shouldBeIgnored!(U, n)) {
-					static if (isAnId!(U, n)) {
-						static if (isAnObjectType!(typeof(mixin("t." ~ m ~ "." ~ n)))) {
-						} else {
-							string name1 = getNameValue!(T, m);
-							string name2 = getNameValue!(U, n);
-							props ~= "this." ~ name1 ~ "_" ~ name2 ~ ", ";
-						}
-					}
-				}
-			}
-			if (props[$-2] == ',')
-				props.length -= 2;
-			props ~= ");\n";
-			props ~= "    },\n";
-			
-			props ~= "    " ~ setterName!(getNameValue!(T, m)) ~ ": function(value) {\n";
-			foreach(n; __traits(allMembers, U)) {
-				static if (isUsable!(U, n) && !shouldBeIgnored!(U, n)) {
-					static if (isAnId!(U, n)) {
-						static if (isAnObjectType!(typeof(mixin("t." ~ m ~ "." ~ n)))) {
-						} else {
-							string name1 = getNameValue!(T, m);
-							string name2 = getNameValue!(U, n);
-							props ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = value." ~ getRelationshipPropertyName!(T, m) ~ "_" ~ name2 ~ ";\n";
-						}
-					}
-				}
-			}
-			props ~= "    },\n";
+			handleClassPropertyRelationshipJSFace!(T, ajaxProtection, m)(ret, constructorArgs, constructorSet, props);
 			break;
 		default:
 			break;
---
+++ source/cmsed/base/internal/generators/js/model/jsface.d
@@ -0,0 +1,109 @@
+module cmsed.base.internal.generators.js.model.jsface;
+import cmsed.base.internal.generators.js.model.defs;
+import dvorm.util;
+
+void handleClassStartJSFace(T, ushort ajaxProtection, T t = newValueOfType!T)(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
+	ret ~= "var " ~ getTableName!T ~ " = Class({\n";
+	constructorArgs ~= "    constructor: function(";
+}
+
+void handleClassEndJSFace(T, ushort ajaxProtection, T t = newValueOfType!T)(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
+	if (constructorArgs[$-2] == ',')
+		constructorArgs.length -= 2;
+	constructorArgs ~= ") {\n";
+	
+	ret ~= constructorArgs;
+	ret ~= constructorSet;
+	ret ~= "    },\n";
+	
+	if (props.length > 2) {
+		if (props[$-2] == ',') {
+			props.length -= 2;
+			props ~= '\n';
+		}
+	}
+	ret ~= props;
+	
+	if (ret[$-2] == ',') {
+		ret.length -= 2;
+		ret ~= '\n';
+	}
+	
+	ret ~= "});";
+}
+
+void handleClassPropertyObjectPropertyJSFace(T, ushort ajaxProtection, string m, string n, // params
+										     T t = newValueOfType!T, U = typeof(mixin("t." ~ m)), V = typeof(mixin("t." ~ m ~ "." ~ n)) // meta info that is needed but not available inside the function
+											 )(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
+	string name1 = getNameValue!(T, m);
+	string name2 = getNameValue!(U, n);
+	
+	constructorArgs ~= name1 ~ "_" ~ name2 ~ ", ";
+	static if (isBoolean!V) {
+		constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = Boolean(" ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ");\n";
+	} else static if (isBasicType!V) {
+		constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = Number(" ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ");\n";
+	} else {
+		constructorSet ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = " ~ name1 ~ "_" ~ name2 ~ " === undefined ? \"" ~ getDefaultValue!(U, n) ~ "\" : " ~ name1 ~ "_" ~ name2 ~ ";\n";
+	}									   
+}
+
+void handleClassPropertyJSFace(T, ushort ajaxProtection, string m, // params
+							   T t = newValueOfType!T, U = typeof(mixin("t." ~ m)) // meta info that is needed but not available inside the function
+							   )(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
+	string name = getNameValue!(T, m);
+	if (name == "")
+		name = "_";
+	
+	constructorArgs ~= name ~ ", ";
+	
+	static if (isBoolean!U) {
+		constructorSet ~= "        this." ~ name ~ " = Boolean(" ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ");\n";
+	} else static if (isBasicType!U) {
+		constructorSet ~= "        this." ~ name ~ " = Number(" ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ");\n";
+	} else {
+		constructorSet ~= "        this." ~ name ~ " = " ~ name ~ " === undefined ? \"" ~ getDefaultValue!(T, m) ~ "\" : " ~ name ~ ";\n";
+	}
+}
+
+void handleClassPropertyRelationshipJSFace(T, ushort ajaxProtection, string m, // params
+										   T t = newValueOfType!T, U = typeof(mixin("t." ~ m)) // meta info that is needed but not available inside the function
+											)(ref string ret, ref string constructorArgs, ref string constructorSet, ref string props) {
+	mixin("import " ~ getRelationshipClassModuleName!(T, m) ~ ";");
+	string name = getTableName!(mixin(getRelationshipClassName!(T, m)));
+	
+	props ~= "    " ~ getterName!(getNameValue!(T, m)) ~ ": function() {\n";
+	props ~= "        return " ~ name ~ ".findOne(";
+	
+	foreach(n; __traits(allMembers, U)) {
+		static if (isUsable!(U, n) && !shouldBeIgnored!(U, n)) {
+			static if (isAnId!(U, n)) {
+				static if (isAnObjectType!(typeof(mixin("t." ~ m ~ "." ~ n)))) {
+				} else {
+					string name1 = getNameValue!(T, m);
+					string name2 = getNameValue!(U, n);
+					props ~= "this." ~ name1 ~ "_" ~ name2 ~ ", ";
+				}
+			}
+		}
+	}
+	if (props[$-2] == ',')
+		props.length -= 2;
+	props ~= ");\n";
+	props ~= "    },\n";
+	
+	props ~= "    " ~ setterName!(getNameValue!(T, m)) ~ ": function(value) {\n";
+	foreach(n; __traits(allMembers, U)) {
+		static if (isUsable!(U, n) && !shouldBeIgnored!(U, n)) {
+			static if (isAnId!(U, n)) {
+				static if (isAnObjectType!(typeof(mixin("t." ~ m ~ "." ~ n)))) {
+				} else {
+					string name1 = getNameValue!(T, m);
+					string name2 = getNameValue!(U, n);
+					props ~= "        this." ~ name1 ~ "_" ~ name2 ~ " = value." ~ getRelationshipPropertyName!(T, m) ~ "_" ~ name2 ~ ";\n";
+				}
+			}
+		}
+	}
+	props ~= "    },\n";
+}
