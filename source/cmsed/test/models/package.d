49b035ae7f7587b829d5629a332a5e15000a9445
rikki cattermole <pbdadmin@gmail.com>
2014-02-09 17:50:57 +1300

    Fixes to keep inline with current standard and reality.

--- source/cmsed/test/models/package.d
+++ source/cmsed/test/models/package.d
@@ -1,9 +1,19 @@
 module cmsed.test.models;
 import cmsed.base.registration.model;
+import cmsed.base.registration.onload;
 
 public import cmsed.test.models.book;
 
 shared static this() {
 	registerModel!Book3;
 	registerModel!Page3;
+	
+	void func(bool isInstall) {
+		Book3 book = new Book3;
+		book.key.isbn = "AS-DF-TF";
+		book.edition = 8;
+		book.save();
+	}
+	
+	registerOnLoad(&func);
 }
