78db215f84dda05834efcdf123da4c8a15178ee2
rikki cattermole <pbdadmin@gmail.com>
2014-04-20 20:55:09 +1200

    Adding caching for policies

---
+++ source/user/cmsed/user/caches/package.d
@@ -0,0 +1,9 @@
+﻿module cmsed.user.caches;
+public import cmsed.user.caches.policy;
+public import cmsed.user.caches.grouppolicy;
+
+/**
+ * All policies are cached,
+ * All group policies are cached.
+ * User policies will not be cached because users change too frequently to cache this information.
+ */
---
+++ source/user/cmsed/user/caches/policy.d
@@ -0,0 +1,25 @@
+﻿module cmsed.user.caches.policy;
+import cmsed.user.models.policy;
+import cmsed.base;
+
+mixin CacheManager!(PolicyModel, "getPolicies");
+
+/**
+ * Gets a policy by name
+ * 
+ * Params:
+ * 		name = 		The name of the policy to get
+ * 
+ * Returns:
+ * 		A data model containing the policy. Which can be updated.
+ */
+PolicyModel getPolicyByName(string name) {
+	synchronized {
+		foreach(s; getPolicies) {
+			if (s.key.name == name)
+				return s;
+		}
+		
+		return null;
+	}
+}
