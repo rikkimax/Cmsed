07fb1694075c1d2d042af77acfc1fd6596a0f4fa
rikki cattermole <pbdadmin@gmail.com>
2014-05-09 03:41:36 +1200

    Adding query cache mechanism.

--- source/base/cmsed/base/cache.d
+++ source/base/cmsed/base/cache.d
@@ -1,4 +1,6 @@
 module cmsed.base.cache;
+import dvorm;
+import std.traits : moduleName;
 
 /**
  * Takes an ORM model, 
@@ -6,8 +8,7 @@ module cmsed.base.cache;
  * Updates as per update mechanism.
  * Includes an on update event call if compilable.
  */
-
-mixin template CacheManager(T, string getName = "get", bool frequent = false) if (
+template CacheManager(T, string getName = "get", bool frequent = false) if (
 	__traits(compiles, {T[] all = T.findAll();})) {
 	
 	private {
@@ -16,6 +17,44 @@ mixin template CacheManager(T, string getName = "get", bool frequent = false) if
 		mixin("void " ~ getName ~ q{Update() {
 				synchronized {
 					store = cast(shared)T.findAll();
+					mixin("static if (__traits(compiles, {" ~ getName ~ "OnUpdate(); })) {" ~ q{
+						mixin(getName ~ "OnUpdate();");
+					} ~ "}");
+				}
+			}});
+	}
+	
+	mixin("T[] " ~ getName ~ q{() {
+			synchronized {
+				return cast(T[])store;
+			}
+		}});
+	
+	mixin(q{
+		shared static this() {
+			import cmsed.base.registration.update;
+			
+			registerUpdate(&mixin(getName ~ "Update"), frequent);
+		}
+	});
+}
+
+/**
+ * Caches a selected result from a given query.
+ * Must be a Dvorm query.
+ * 
+ * Usage:
+ * 		mixin CacheQuery!(MyModel, q{MyModel.query().myproperty_eq(0)}, "getModelValues", true);
+ */
+template CacheQuery(T, string query, string getName = "get", bool frequent = false) {
+	static assert(__traits(compiles, {T[] values = mixin(query ~ ".find()");}), "Query for " ~ T.stringof ~ " does not compiled: " ~ query ~ ".find(). Return value must be of type: " ~ T.stringof ~ "[]");
+	
+	private {
+		shared T[] store;
+		
+		mixin("void " ~ getName ~ q{Update() {
+				synchronized {
+					store = cast(shared)mixin(query).find();
 					mixin("static if (__traits(compiles, {" ~ getName ~ "OnUpdate(); })) {" ~ q{
 						mixin(getName ~ "OnUpdate();");
 					} ~ "}");
